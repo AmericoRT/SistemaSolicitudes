f2ef1dcf39e7c1f969cfeb182a1fe8d40dd1d19d
Americo <americo.ramos.1999@gmail.com>
2025-06-10 19:34:37 -0500

    Relaizacion validacion API en el registro

--- ClienteWeb/Controllers/RegistroController.cs
+++ ClienteWeb/Controllers/RegistroController.cs
@@ -1,11 +1,28 @@
-﻿using System.Web.Mvc;
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 using LogicaNegocio;
+using Newtonsoft.Json;
 
 namespace SistemaSolicitudes.ClienteWeb.Controllers
 {
     public class RegistroController : Controller
     {
         private readonly UsuarioService servicio = new UsuarioService();
+
+        private async Task<bool> VerificarDniEnApiAsync(string dni)
+        {
+            using (var client = new HttpClient())
+            {
+                string apiUrl = $"http://localhost:7756/api/asegurados/{dni}"; 
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("X-Api-Key", "12345ABC"); // Cambia por tu clave real
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                return response.IsSuccessStatusCode;
+            }
+        }
 
         [HttpGet]
         public ActionResult Index()
@@ -14,7 +31,7 @@ namespace SistemaSolicitudes.ClienteWeb.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(string DNI, string Codigo, string ConfirmarCodigo)
+        public async Task<ActionResult> Index(string DNI, string Codigo, string ConfirmarCodigo)
         {
             if (string.IsNullOrWhiteSpace(DNI) || DNI.Length < 8 || DNI.Length > 12)
             {
@@ -25,6 +42,13 @@ namespace SistemaSolicitudes.ClienteWeb.Controllers
             if (Codigo != ConfirmarCodigo)
             {
                 ViewBag.Mensaje = "Las contraseñas no coinciden.";
+                return View();
+            }
+
+            bool dniValido = await VerificarDniEnApiAsync(DNI);
+            if (!dniValido)
+            {
+                ViewBag.Mensaje = "El DNI no se encuentra registrado como asegurado.";
                 return View();
             }
 
