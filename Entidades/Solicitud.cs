39b3264ecb7703a0d9a518fa568746c25198d008
Americo <americo.ramos.1999@gmail.com>
2025-06-19 18:26:00 -0500

    prueba 2

---
+++ Entidades/Solicitud.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Solicitud
+    {
+        public int Id { get; set; }
+        public string TipoSolicitud { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaSolicitud { get; set; }
+        public string DocumentoAdjuntoRuta { get; set; }
+    }
+}
